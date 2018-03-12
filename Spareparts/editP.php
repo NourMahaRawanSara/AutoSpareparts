fc56377c4e3173c900419f0a2429f8677785b73b
NourMahaRawanSara <36901160+NourMahaRawanSara@users.noreply.github.com>
2018-03-12 21:54:05 +0200

    push

--- Spareparts/editP.php
+++ Spareparts/editP.php
@@ -1,26 +1,71 @@
 <?php
-session_start();
-require("user.php");
-
-function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
-
-if (isset($_SESSION["ID"])){
-    $user = new User($_SESSION["ID"]);
-}
-
-if (isset($_POST['edit'])){
-    $firstname = test_input($_POST["fname"]);
-    $lastname = test_input($_POST["lname"]);
-    $username = test_input($_POST["email"]);
-    $password = test_input($_POST["password"]);
-    $gender = test_input($_POST["gender"]);
-
-        if($user->editInfo($firstname,  $lastname, $username , $password)){
-        echo 'updated'; }
- }
+  session_start();
+  require("ConnectionToDB.php");
 ?>
+<!DOCTYPE html>
+    <head>
+
+    </head>
+<html>
+    <body>
+        <form method="post" action="">
+
+          <strong>First Name:<strong><br>
+         <input type="text" name="fname" placeholder="Firstname" required><br>
+
+        <strong>Last Name:<strong><br>
+         <input type="text" name="Lname" placeholder="Lastname" required><br>
+
+         <strong>Email:<strong><br>
+           <input type="email" name="Email" placeholder="Email" required><br>
+
+           <strong>Username:<strong><br>
+            <input type="text" name="Username" placeholder="Username" required><br>
+
+
+           <strong>Password:<strong><br>
+             <input type="password" name="password" placeholder="Password" required><br>
+
+             <strong>Mobile Number:<strong><br>
+               <input type="tel" name="Mobile" placeholder="Mobile Number" required><br>
+
+
+          <strong>Birthdate:</strong><br>
+                <input type="date" name="DateOfBirth"><br>
+          <br>
+
+              <input type="submit" name="submit" value="Submit">
+              <input type="reset" value="Cancel">
+              </div>
+                    </form>
+
+              </div>
+        </div>
+        </div>
+<?php
+    if (isset($_POST['submit'])){
+      $fname = $_POST['fname'];
+      $lname=$_POST['Lname'];
+      $dob = $_POST['DateOfBirth'];
+      $mobile=$_POST['Mobile'];
+      $email = $_POST['Email'];
+      //$usertype_id = $_POST['UserTypeID'];
+      $username=$_POST['Username'];
+      $password = $_POST['password'];
+
+      $user = new User;
+      $user->firstname = $fname;
+      $user->lastname=$lname;
+      $user->DOB = $dob;
+      $user->mobile=$mobile;
+      $user->email = $email;
+      //$user->$usertype_id = $usertype;
+      $user->username=$username;
+      $user->password = $password;
+      $user->insertInDb($fname,$lname,$dob,$mobile,$email,
+    /*$usertype,*/$username,$password);
+
+  }
+?>
+</body>
+</html>
