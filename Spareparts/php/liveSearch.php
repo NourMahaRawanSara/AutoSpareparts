6d29450ea187f00d6e890fef10bedafa4968373a
NourMahaRawanSara <36901160+NourMahaRawanSara@users.noreply.github.com>
2018-05-08 23:45:40 +0200

    .

--- Spareparts/php/liveSearch.php
+++ Spareparts/php/liveSearch.php
@@ -3,15 +3,15 @@
 		session_start();
 		require("ConnectionToDB.php");
 
-		$db = new dbconnect;
+   $db = ConnectionToDB::getInstance();
+   $mysqli = $db->getConnection();
 
-		$querySearch = $_GET["q"];
+   $querySearch = $_GET["q"];
 
 		$queryStatement = "SELECT * FROM user WHERE 'Fname' LIKE '%$querySearch%' OR 'Lname' LIKE '%$querySearch%'";
 
-		$result = $db->executesql($queryStatement);
-
-		while($row = mysqli_fetch_assoc($result)){
+   $result = $mysqli->query($queryStatement);
+		while($row =mysqli_fetch_array($result)){
 			echo $row["Fname"] . " " . $row["Lname"] . "<br>";
 		}
 
