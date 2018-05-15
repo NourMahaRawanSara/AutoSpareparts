f3f98426420521574fe20ad5aacd46f8d7dedacb
mahafayez <37303988+mahafayez@users.noreply.github.com>
2018-05-15 22:49:05 +0200

    Add files via upload

---
+++ Spareparts/BillModel.php
@@ -0,0 +1,91 @@
+<?php
+
+
+  class BillModel{
+
+      public $ID;
+      public $OrderDetailsID;
+      public $SparePartID;
+      public $UserID;
+      public $TotalAmount;
+      public $CommissionID;
+      public $Notes;
+
+
+
+      public function __construct(){
+
+      }
+
+      public function AddBill(){
+          $db = ConnectionToDB::getInstance();
+          $mysqli = $db->getConnection();
+
+          $sql = "INSERT INTO `bill` (`ID`, `OrderDetailsID`, `SparePartID`, 
+                                      `UserID`, `Total Amount`, `CommissionID`, `Notes`) 
+                  VALUES 
+                  (NULL, '$this->OrderDetailsID', '$this->SparePartID', 
+                  '$this->UserID', '$this->TotalAmount', 
+                  '$this->CommissionID', '$this->Notes')
+                  ";
+          $result = $mysqli->query($sql);
+      }
+
+      public function Edit(){
+
+      }
+
+
+      public function calc_stat()
+      {
+          $db = ConnectionToDB::getInstance();
+          $mysqli = $db->getConnection();
+          $sql=" SELECT Distinct bill.`Total Amount`, user.ID from bill inner join user on user.ID = bill.UserID";
+          $result = $mysqli->query($sql);
+          $i=-1;
+
+          while($row =mysqli_fetch_array($result)){
+              $i++;
+              $this->ID[$i]=$row['ID'];
+              $this->TotalAmount[$i]=$row['Total Amount'];
+
+          }
+          return $i;
+      }
+
+      public function View(){
+          $db = ConnectionToDB::getInstance();
+          $mysqli = $db->getConnection();
+
+          $sql = "SELECT * FROM `bill`
+                ";
+          $result = $mysqli->query($sql);
+          $i=-1;
+
+          while($row =mysqli_fetch_array($result)){
+              $i++;
+              $this->ID[$i]=$row['ID'];
+              $this->OrderDetailsID[$i]=$row['OrderDetailsID'];
+              $this->SparePartID[$i]=$row['SparePartID'];
+              $this->UserID[$i]=$row['UserID'];
+              $this->TotalAmount[$i]=$row['Total Amount'];
+              $this->CommissionID[$i]=$row['CommissionID'];
+              $this->Notes[$i]=$row['Notes'];
+          }
+          return $i;
+      }
+
+     /* public function CounterSP(){
+          $db = ConnectionToDB::getInstance();
+          $mysqli = $db->getConnection();
+
+          $sql = "SELECT COUNT(*) FROM sparepart";
+          $result = $mysqli->query($sql);
+          return $result;
+      }*/
+      public function Delete(){
+
+      }
+
+  }
+?>
